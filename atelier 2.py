74d425b43d676a36f023fe8e7adf5d7e2a615f8e
youneschaghica-hub <youneschaghica@gmail.com>
2026-03-09 21:19:38 -0400

    les méthodes de la classe « Employe »

--- atelier 2.py
+++ atelier 2.py
@@ -4,3 +4,28 @@ class Employe:
         self.nom = nom
         self.prenom = prenom
         self.voitureService = None
+
+    def afficherInformations(self):
+        print(f"Employé : {self.nom} {self.prenom}, numéro Permis : {self.numeroPermis}")
+        if self.voitureService:
+            print("Voiture attribuée :")
+            self.voitureService.afficherInformations()
+        else:
+            print("Aucune voiture attribuée.")
+
+    def affecterVoiture(self, voiture):
+        if voiture.chauffeur is not None:
+            print(f"Erreur : la voiture {voiture.matricule} est déjà attribuée à {voiture.chauffeur.nom}.")
+        else:
+            if self.voitureService:
+                print(f"Attention : l'employé {self.nom} avait déjà une voiture, elle sera remplacée.")
+                self.voitureService.chauffeur = None
+            self.voitureService = voiture
+            voiture.chauffeur = self
+
+    def retirerVoiture(self):
+        if self.voitureService:
+            self.voitureService.chauffeur = None
+            self.voitureService = None
+        else:
+            print(f"{self.nom} n'a pas de voiture à retirer.")
