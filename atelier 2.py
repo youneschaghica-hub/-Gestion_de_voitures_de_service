eb7f25675fd806ee378ddb6221353f094634808f
youneschaghica-hub <youneschaghica@gmail.com>
2026-03-09 21:14:17 -0400

    la classe Employe et ses attributs

---
+++ atelier 2.py
@@ -0,0 +1,6 @@
+class Employe:
+    def __init__(self, numeroPermis, nom, prenom):
+        self.numeroPermis = numeroPermis
+        self.nom = nom
+        self.prenom = prenom
+        self.voitureService = None
